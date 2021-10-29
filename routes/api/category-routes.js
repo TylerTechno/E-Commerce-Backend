c4636edc5392ab1f86341b92784e9409776b7b6f
Tyler Everett <tylertechnotrade@gmail.com>
2021-10-29 16:21:35 -0700

    another commit

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,18 +20,64 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: Product
+  })
+    .then(category_data => {
+      res.json(category_data)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+    .then(category_data => {
+      res.json(category_data)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(category_data => {
+    res.json(category_data)
+  })
+  .catch(error => {
+    res.status(500).json(error)
+  })
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+
+  })
+    .then(category_data => {
+      res.json(category_data)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+
 });
 
 module.exports = router;
